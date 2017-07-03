03ba8225efdaced8cd149343b092f938e1bd0afb
FelixWaweru <wawerufelix60@gmail.com>
2017-07-03 10:11:13 +0300

    Saving of contact name and number added

--- app/src/main/java/yousalama/com/yousalama/Settings.java
+++ app/src/main/java/yousalama/com/yousalama/Settings.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -13,15 +15,31 @@ import android.widget.TextView;
  */
 public class Settings extends AppCompatActivity{
     EditText phoneNo1;
+    EditText contactname;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        contactname=(EditText) findViewById(R.id.name);
         phoneNo1=(EditText) findViewById(R.id.number1);
-        Intent intent= new Intent(Settings.this,MainActivity.class);
-        String str=phoneNo1.getText().toString();
-        TextView mTextView = (TextView) findViewById(R.id.textt);
-        mTextView.setText(str);
-        intent.putExtra("Number",str);
+        Button Save = (Button) findViewById(R.id.save);
+        Save.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String str = contactname.getText().toString();
+                        String str2 = phoneNo1.getText().toString();
+                        TextView namee = (TextView) findViewById(R.id.namee);
+                        namee.setText(str);
+                        TextView number = (TextView) findViewById(R.id.textt);
+                        number.setText(str2);
+                        Intent intent = new Intent(Settings.this, MainActivity.class);
+                        intent.putExtra("Name", str);
+                        Intent intent2 = new Intent(Settings.this, MainActivity.class);
+                        intent2.putExtra("Number", str2);
+                    }
+                }
+        );
+
     }
 
     @Override
